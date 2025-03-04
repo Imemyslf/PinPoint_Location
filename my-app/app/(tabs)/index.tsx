746f4d092b38861e6030f29c7be0500479198d72
Valerian Coelho <valerian27012004@gmail.com>
2025-03-04 11:36:31 +0530

    Body Image added

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 
 export default function Tab() {
   return (
     <View style={styles.container}>
-      <Text>Body Suit</Text>
+      <Image source={require("../../assets/images/body_suit/body_suit.png")} style={styles.image} />
+      <Image source={require("../../assets/images/body_suit/body_suit.png")} style={styles.image} />
+      <TextInput placeholder="Type here" style={styles.input} />
     </View>
   );
 }
@@ -11,7 +13,25 @@ export default function Tab() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  image: {
+    height: "40%",
+    borderRadius: 10,
+    resizeMode: "contain", // Ensures the whole image fits inside the box
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
   },
 });
